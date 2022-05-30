df19833b4159d95ff0ea722816f826bb4cbe583b
hui61 <huiliuyi@gmail.com>
2022-05-30 14:34:07 +0800

    Spring DI

--- src/test/java/demo/TestSpring.java
+++ src/test/java/demo/TestSpring.java
@@ -14,4 +14,15 @@ public class TestSpring {
         // 3.调用方法
         userService.print();
     }
+
+    @Test
+    void test_DI() {
+        // 1、获取工厂
+        ApplicationContext act = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        // 2、通过工厂类获得对象
+        User user = (User)act.getBean("user");
+        System.out.println("姓名："+user.getName());
+        System.out.println("性别："+user.getAge());
+
+    }
 }
